3087dda25d9c8ff159499a0d726cf1c58690983f
dickyfahlevi21 <dicky.fahlevi21@gmail.com>
2020-08-07 02:07:59 +0700

    progress baru sampai create db

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+const rootIndex = require('./src/routes/index')
+const routerAuthor = require('./src/routes/author')
+
+app.use('/', rootIndex)
+app.use('/author', routerAuthor)
+
 
 app.listen(port, () => {
     console.log(`example at http://localhost:${port}`)
